d7d77517787de1f11765875faf2741e4324d86b8
rodrigoyr <ryeperub@myuax.com>
2024-02-07 18:09:47 +0100

    commit ej2

---
+++ Ejercicio2.java/src/main/java/org/example/MaximosEnteros.java
@@ -0,0 +1,16 @@
+public class MaximosEnteros {
+
+    public static void main(String[] args) {
+        // Mayor número entero representable por un char
+        char maxChar = Character.MAX_VALUE;
+        System.out.println("Mayor número entero representable por un char: " + (int) maxChar);
+
+        // Mayor número entero representable por un short
+        short maxShort = Short.MAX_VALUE;
+        System.out.println("Mayor número entero representable por un short: " + maxShort);
+
+        // Mayor número entero representable por un int
+        int maxInt = Integer.MAX_VALUE;
+        System.out.println("Mayor número entero representable por un int: " + maxInt);
+    }
+}
